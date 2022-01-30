648e9807eb15daf8ae002250bfecd5d7edc30451
Krupesh Bhosle <bhoslekrupesh03@gmail.com>
2022-01-30 23:55:33 +0530

    search field styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
 </head>
@@ -47,7 +48,7 @@
                         <a href="#" class="nav-link">Venues</a>
                     </li>
                     <li class="nav-item">
-                        <a href="{{ url('/fixture') }}" class="nav-link">Recent Schedule/Results</a>
+                        <a href="{{ url('/fixture') }}" class="nav-link">Schedule/Results</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Live Scores</a>
@@ -65,12 +66,11 @@
                 </ul>
                 <ul class="nav navbar-nav ms-auto">
                     <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Search</a>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <a href="#" class="dropdown-item">Reports</a>
-                            <a href="#" class="dropdown-item">Settings</a>
-                            <div class="dropdown-divider"></div>
-                            <a href="#" class="dropdown-item">Logout</a>
+                        <div class="search-container">
+                            <form action="return false">
+                            <input type="text" placeholder="Search.." name="search">
+                            <button type="submit"><i class="fa fa-search"></i></button>
+                            </form>
                         </div>
                     </li>
                 </ul>
